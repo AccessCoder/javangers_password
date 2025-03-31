f32c27e7db3c0764d5a0a67220a9a821745e7f02
AccessCoder <info.unaddict@gmail.com>
2025-03-31 16:39:01 +0200

    add first iteration of Method checkLenght

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,4 +13,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean checkLength(String password) {
+        return true;
+    }
 }
