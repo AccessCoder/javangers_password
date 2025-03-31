32b4c4f4eacb28065402bc9516fb8e365facc28a
AccessCoder <info.unaddict@gmail.com>
2025-03-31 16:53:35 +0200

    refactor containsUppercase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,6 +26,6 @@ public class Main {
     }
 
     public static boolean containsUppercase(String password) {
-        return true;
+        return !password.toLowerCase().equals(password);
     }
 }
