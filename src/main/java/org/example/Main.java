46a608f2bdbfdcfebebeee6584d157e60961e275
AccessCoder <info.unaddict@gmail.com>
2025-03-31 16:37:55 +0200

    add first Test for checkLenght

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,13 @@
 package org.example;
 
+/**
+ *
+ * A method that ensures a password is at least 8 characters long.
+ * With another method, you want to check if the password contains digits.
+ * Additionally, you want to ensure with a method that both uppercase and lowercase letters are used in the password.
+ * And a method should detect commonly used passwords.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ *
+ * A method that ensures a password is at least 8 characters long.
+ * With another method, you want to check if the password contains digits.
+ * Additionally, you want to ensure with a method that both uppercase and lowercase letters are used in the password.
+ * And a method should detect commonly used passwords.
+ */
+class MainTest {
+
+    @Test
+    void checkLength_shouldReturnTrue_whenCalledWithPasswordWith8Characters() {
+        //GIVEN
+        String password = "Pass1213";
+        //WHEN
+        boolean actual = Main.checkLength(password);
+        //THEN
+        assertTrue(actual);
+
+    }
+}
